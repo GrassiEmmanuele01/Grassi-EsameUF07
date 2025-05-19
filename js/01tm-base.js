57712bcd554e88b07fd48c2d6090172170f504c6
GrassiEmmanuele <emmanuele.grassi@mat.tn.it>
2025-05-19 14:08:17 +0200

    feat(01tm-base):aggiunta funzionalità per eliminare, ripristinare e eliminare definitivamente attività

--- js/01tm-base.js
+++ js/01tm-base.js
@@ -1,4 +1,5 @@
 const tasks = [];
+const deletedTasks = [];
 
 function addTask() {
   const taskInput = document.getElementById("inputTask");
@@ -14,10 +15,34 @@ function addTask() {
   taskInput.value = "";
 }
 
+function removeTask(index) {
+  const removedTask = tasks.splice(index, 1)[0];
+  deletedTasks.push(removedTask);
+  renderTasks();
+}
+
+function restoreTask(deletedIndex) {
+  const restoredTask = deletedTasks.splice(deletedIndex, 1)[0];
+  tasks.push(restoredTask);
+  renderTasks();
+}
+
+function deletePermanently(deletedIndex) {
+  if (
+    confirm("Sei sicuro di voler rimuovere definitivamente questa attività?")
+  ) {
+    deletedTasks.splice(deletedIndex, 1);
+    renderTasks();
+  }
+}
+
 function renderTasks() {
   const taskItems = document.getElementById("taskItems");
 
+  const deletedList = document.getElementById("deletedList");
+
   taskItems.innerHTML = "";
+  deletedList.innerHTML = "";
 
   tasks.forEach((task, index) => {
     const taskItem = document.createElement("li");
@@ -27,5 +52,17 @@ function renderTasks() {
         <button class="remove-task" onclick="removeTask(${index})">Rimuovi ❌</button>
       `;
     taskItems.appendChild(taskItem);
+  });
+
+  deletedTasks.forEach((task, index) => {
+    const deletedItem = document.createElement("li");
+    deletedItem.innerHTML = `
+        <span>${task}</span>
+        <div>
+          <button class="restore-task" onclick="restoreTask(${index})">Ripristina 🔄</button>
+          <button class="delete-permanently" onclick="deletePermanently(${index})">Rimuovi definitivamente 🗑️</button>
+        </div>
+      `;
+    deletedList.appendChild(deletedItem);
   });
 }
